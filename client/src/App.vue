b206875195a501f6c6e0ea4425fdf512492d98fb
Alexander Khodzinsky <net1984master@gmail.com>
2022-05-25 10:15:08 +0300

    массивы в VUE

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,37 @@
 <template>
-
+  <div>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название</strong> {{ post.title }}</div>
+      <div><strong>Описание</strong> {{ post.body }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "App"
-}
+  data() {
+    return {
+      posts: [
+        { id: 1, title: 'JavaScript', body: 'description' },
+        { id: 2, title: 'JavaScript', body: 'description' },
+        { id: 3, title: 'JavaScript', body: 'description' },
+      ],
+    };
+  },
+  methods: {},
+};
 </script>
 
-<style scoped>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
 </style>

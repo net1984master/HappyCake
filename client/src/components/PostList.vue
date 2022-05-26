bd20a4ca3fac7f470082c7d79a37b6495e253173
Alexander Khodzinsky <net1984master@gmail.com>
2022-05-26 10:40:31 +0300

    анимация на удаление и добавление постов

--- client/src/components/PostList.vue
+++ client/src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
-<div v-if="posts.length>0">
-  <post-item
-      v-for="post in posts"
-      :post="post"
-      :key="post.id"
-      @remove="$emit('remove', post)"
-  />
-</div>
+  <div v-if="posts.length > 0">
+    <transition-group name="post-list">
+      <post-item
+        v-for="post in posts"
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
+  </div>
   <h2 v-else>Постов нет</h2>
 </template>
 
@@ -27,4 +29,19 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+
+</style>

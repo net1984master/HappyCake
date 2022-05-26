7535cc6668908c5240564b8cfcb73cbee721be9e
Alexander Khodzinsky <net1984master@gmail.com>
2022-05-26 15:05:59 +0300

    динамическая навигация

---
+++ client/src/pages/PostPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Это пост с ID = {{ $route.params.id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostPage',
+};
+</script>
+
+<style scoped></style>
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Main from '@/pages/Main.vue';
 import PostsPage from '@/pages/PostsPage.vue';
+import PostPage from '@/pages/PostPage.vue';
 import About from '@/pages/About.vue';
 
 const routes: Array<RouteRecordRaw> = [
@@ -13,6 +14,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/posts',
     name: 'posts',
     component: PostsPage,
+  },
+  {
+    path: '/posts/:id',
+    name: 'post',
+    component: PostPage,
   },
   {
     path: '/about',

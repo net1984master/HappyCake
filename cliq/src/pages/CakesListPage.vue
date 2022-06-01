11a5416fa210b705bb579e284a4e285df0934a44
Alexander Khodzinsky <net1984master@gmail.com>
2022-06-01 15:02:29 +0300

    добавил слот фильтра

--- cliq/src/pages/CakesListPage.vue
+++ cliq/src/pages/CakesListPage.vue
@@ -2,27 +2,46 @@
   <q-page>
     <div class="q-pa-md">
       <q-table
-        class="my-sticky-header-table"
+        class="grid-style-transition"
         title="Торты"
         :rows="cakes"
         :columns="columns"
         row-key="name"
         flat
         bordered
-      />
+        :filter="filter"
+      >
+        <template v-slot:top-right="props">
+          <!--            Примеры фильтра-->
+          <!--            https://codepen.io/b0otable/pen/PozWLYR?editors=1010-->
+          <!--          https://codepen.io/metalsadman/pen/ZgKexK-->
+          <q-input
+            v-model="filter"
+            outlined
+            denseola
+            debounce="300"
+            placeholder="Фильтр">
+            <template v-slot:append>
+              <q-icon name="search"/>
+            </template>
+          </q-input>
+        </template>
+
+      </q-table>
     </div>
   </q-page>
 </template>
 
 <script>
-import {computed, onMounted} from "vue";
+import {computed, onMounted, ref} from "vue";
 import {useStore} from "vuex";
 
 export default {
   name: 'CakesListPage',
   setup() {
     const store = useStore();
-    const cakes = computed(()=>store.state.qcakem.cakes);
+    const cakes = computed(() => store.state.qcakem.cakes);
+    const filter = ref('');
     const columns = [
       {
         name: 'name',
@@ -30,24 +49,26 @@ export default {
         label: 'id',
         align: 'left',
         field: 'id',
-        format: val => `${val}`,
         sortable: true
       },
-      { name: 'name', align: 'left', label: 'Название', field: 'name', sortable: true },
-      { name: 'description', align: 'left', label: 'Описание', field: 'description', sortable: true },
+      {name: 'name', align: 'left', label: 'Название', field: 'name', sortable: true},
+      {name: 'description', align: 'left', label: 'Описание', field: 'description', sortable: true},
     ];
-    onMounted(()=>{
+    onMounted(() => {
       store.dispatch('qcakem/getAll');
     });
     return {
       columns,
-      cakes
+      cakes,
+      filter
     };
 
   }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.grid-style-transition {
+  transition: transform .28s, background-color .28s
+}
 </style>
